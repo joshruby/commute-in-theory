2b512d86da4ec8d6b06a6b0ddfcfeff95331766c
joshua-ruby <josh.r.ruby@gmail.com>
2021-12-12 20:11:15 -0800

    Implement recording function

---
+++ recording-util-python/recording-util.py
@@ -0,0 +1,60 @@
+import json
+import requests
+import config
+
+def recordCommute(commute_request):
+    # Build the URL
+    uri = f'https://api.tomtom.com/routing/1/calculateRoute/{commute_request["origin"]["lat_lon"]}:{commute_request["destination"]["lat_lon"]}/json'
+    params = {
+        'sectionType': 'traffic',
+        'traffic': 'true',
+        'travelMode': 'car',
+        'vehicleCommercial': 'false',
+        'vehicleEngineType': 'combustion',
+        'report': 'effectiveSettings',
+        'key': config.TOMTOM_KEY
+    }
+
+    # Call the API
+    res = requests.get(uri, params=params)
+
+    # Convert response to JSON
+    res = res.json()
+
+    # Keep only the relevant information
+    return {
+        'origin': commute_request['origin'],
+        'destination': commute_request['destination'],
+        'departureTime': res['routes'][0]['summary']['departureTime'],
+        'travelTimeInSeconds': res['routes'][0]['summary']['travelTimeInSeconds']
+    }
+
+LOCATIONS = {
+    'MTV': {
+        'name': 'Mountain View',
+        'lat_lon': '37.403712687363814,-122.07814772790742'
+    },
+    'SCZ': {
+        'name': 'Santa Cruz, Downtown',
+        'lat_lon': '36.974797779340655,-122.0290861946705'
+    },
+    'CUP': {
+        'name': 'Cupertino',
+        'lat_lon': '37.330227595678146,-122.03281591229046'
+    },
+    'STA': {
+        'name': 'Stanford',
+        'lat_lon': '37.42666339942809,-122.17649144765893'
+    },
+}
+
+if __name__ == "__main__":
+    commute = recordCommute(
+        {
+            'origin': LOCATIONS['MTV'],
+            'destination': LOCATIONS['CUP']
+        }
+    )
+
+    text = json.dumps(commute, sort_keys=True, indent=4)
+    print(text)
