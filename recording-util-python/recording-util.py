62dd7d8ff48bb617bb6d81548e5791b8421470be
joshua-ruby <josh.r.ruby@gmail.com>
2021-12-13 14:34:47 -0800

    Reduce locations to stay within TomTom free tier

--- recording-util-python/recording-util.py
+++ recording-util-python/recording-util.py
@@ -58,22 +58,11 @@ LOCATIONS = {
         },
     },
     'home': {
-        'MTV': {
-            'name': 'Mountain View, Shoreline',
-            'lat_lon': '37.403712687363814,-122.07814772790742'
-        },
         'SCZ': {
             'name': 'Santa Cruz, Downtown',
             'lat_lon': '36.974797779340655,-122.0290861946705'
         },
-        'CPA': {
-            'name': 'Capitola, Mall',
-            'lat_lon': '36.97371780004841,-121.95931307716036'
-        },
-        'FEL': {
-            'name': 'Felton, Downtown',
-            'lat_lon': '37.05131035573837,-122.07406272553854'
-        },
+  
         'CAM': {
             'name': 'Campbell, Downtown',
             'lat_lon': '37.28750197556045,-121.94487365182788'
@@ -86,34 +75,47 @@ LOCATIONS = {
             'name': 'Santa Jose, Downtown',
             'lat_lon': '37.33481534485126,-121.88822099401446'
         },
-        'PCA': {
-            'name': 'Pacifica, Linda Mar',
-            'lat_lon': '37.594122784571354,-122.5015289019967'
-        },
-        'SFM': {
-            'name': 'San Francisco, Mission District',
-            'lat_lon': '37.75870633260038,-122.41794365829263'
-        },
         'SFP': {
             'name': 'San Francisco, Pacific Heights',
             'lat_lon': '37.79427344559132,-122.43493160658136'
         },
-        'BER': {
-            'name': 'Berkeley, Downtown',
-            'lat_lon': '37.87075376688075,-122.27199562287598'
-        },
+        
         'LGS': {
             'name': 'Los Gatos, Downtown',
             'lat_lon': '37.22473891235269,-121.98375589032867'
-        },
-        'WLG': {
-            'name': 'Willow Glen, Doerr Park',
-            'lat_lon': '37.2739703906996,-121.91368527984521'
         },
         'MLP': {
             'name': 'Menlo Park, West MP',
             'lat_lon': '37.431029416205284,-122.20217060835637'
         },
+        # 'FEL': {
+        #     'name': 'Felton, Downtown',
+        #     'lat_lon': '37.05131035573837,-122.07406272553854'
+        # },
+        # 'WLG': {
+        #     'name': 'Willow Glen, Doerr Park',
+        #     'lat_lon': '37.2739703906996,-121.91368527984521'
+        # },
+        # 'SFM': {
+        #     'name': 'San Francisco, Mission District',
+        #     'lat_lon': '37.75870633260038,-122.41794365829263'
+        # },
+        # 'PCA': {
+        #     'name': 'Pacifica, Linda Mar',
+        #     'lat_lon': '37.594122784571354,-122.5015289019967'
+        # },
+        # 'BER': {
+        #     'name': 'Berkeley, Downtown',
+        #     'lat_lon': '37.87075376688075,-122.27199562287598'
+        # },
+        # 'CPA': {
+        #     'name': 'Capitola, Mall',
+        #     'lat_lon': '36.97371780004841,-121.95931307716036'
+        # },
+        # 'MTV': {
+        #     'name': 'Mountain View, Shoreline',
+        #     'lat_lon': '37.403712687363814,-122.07814772790742'
+        # },
     }
 }
 
