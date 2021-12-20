f9d0f047f395ac8cd5798f7691a5d0283a0c07ad
joshua-ruby <josh.r.ruby@gmail.com>
2021-12-19 17:29:51 -0800

    Complete Lesson 2: Bar Chart

--- commute-in-theory-sveltekit/src/routes/index.svelte
+++ commute-in-theory-sveltekit/src/routes/index.svelte
@@ -1,7 +1,9 @@
 <script>
     import SvgOne from '$lib/components/SvgOne.svelte'
+    import BarTwo from '$lib/components/BarTwo.svelte'
 </script>
 
 <h1>Commute in Theory</h1>
 
-<SvgOne/>
+<!-- <SvgOne/> -->
+<BarTwo/>
