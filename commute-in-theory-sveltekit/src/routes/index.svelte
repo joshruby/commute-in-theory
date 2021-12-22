72351486b4d38e6f249fc7c7f6991e5cd2c70f49
joshua-ruby <josh.r.ruby@gmail.com>
2021-12-21 20:12:29 -0800

    Prepare the lesson 3 code (nothing new yet)

--- commute-in-theory-sveltekit/src/routes/index.svelte
+++ commute-in-theory-sveltekit/src/routes/index.svelte
@@ -1,11 +1,13 @@
 <script>
-    import OneSVG from '$lib/components/OneSVG.svelte'
-    import TwoBar from '$lib/components/TwoBar.svelte'
-    import ThreeScatter from '$lib/components/ThreeScatter.svelte'
+    // import OneSVG from '$lib/components/OneSVG.svelte'
+    // import TwoBar from '$lib/components/TwoBar.svelte'
+    // import ThreeScatter from '$lib/components/ThreeScatter.svelte'
+    import FourMain from '$lib/components/FourMain.svelte'
 </script>
 
 <h1>Commute in Theory</h1>
 
-<!-- <SvgOne/> -->
-<!-- <TwoBar/> -->
-<ThreeScatter/>
+<!-- <SvgOne /> -->
+<!-- <TwoBar /> -->
+<!-- <ThreeScatter /> -->
+<FourMain />
