b0fcd18e3be970349e30decc44ea339ce9111db5
joshua-ruby <josh.r.ruby@gmail.com>
2021-12-22 23:18:10 -0800

    Get commutes, save them in a store, set page cache

--- commute-in-theory-sveltekit/src/lib/stores/CommuteStore.js
+++ commute-in-theory-sveltekit/src/lib/stores/CommuteStore.js
@@ -1,27 +1,3 @@
 import { writable } from 'svelte/store'
-// import clientPromise from '$lib/db'
 
 export const CommuteStore = writable([]);
-
-const fetchCommutes = async () => {
-    // Method 1: fetch data from my own endpoint
-    // Getting 'ERR_INVALID_URL'
-    // const res = await fetch('src/routes/recorded-commutes');
-    // const commutes = await res.json();
-
-    // Method 2: connect to the db directly
-    // Getting "global is undefined" most likely from 
-    // import { MongoClient } from 'mongodb' in db.js
-    // I know the fetch request did make it through to mongo though since it
-    // should up in the db monitor graphs
-    // const connectedClient = await clientPromise;
-    // const db = connectedClient.db();
-    // const collection = db.collection('commutes');
-
-    // // Retrieve db items and put into an array
-    // const commutes = await collection.find().toArray();
-
-    CommuteStore.set(commutes)
-}
-
-fetchCommutes();
--- commute-in-theory-sveltekit/src/routes/index.svelte
+++ commute-in-theory-sveltekit/src/routes/index.svelte
@@ -1,10 +1,37 @@
+<script context="module">
+    import { onMount, onDestroy } from 'svelte';
+    import { CommuteStore } from '$lib/stores/CommuteStore'
+
+    export const load = async ({ page, fetch }) => {
+        // let commutes = [];
+
+        const res = await fetch('/recorded-commutes', { credentials: 'omit' });
+        if (res.ok) {
+            // Cache the page for this many seconds
+            return {
+                props: {
+                    commuteData: await res.json()
+                },
+                maxage: 60 * 15 // Cache the page in the cdn for this many seconds
+            }
+        }
+
+        return {
+            status: res.status,
+            error: new Error('Could not load the recorded commutes.')
+        }
+
+    }
+</script>
+
 <script>
     // import OneSVG from '$lib/components/OneSVG.svelte'
     // import TwoBar from '$lib/components/TwoBar.svelte'
     // import ThreeScatter from '$lib/components/ThreeScatter.svelte'
     // import FourMain from '$lib/components/FourMain.svelte'
 
-    import { CommuteStore } from '$lib/stores/CommuteStore'
+    export let commuteData;
+    CommuteStore.set(commuteData.commutes)
 </script>
 
 <h1>Commute in Theory</h1>
@@ -13,5 +40,7 @@
 <!-- <TwoBar /> -->
 <!-- <ThreeScatter /> -->
 <!-- <FourMain width={600} height={600} xDimension={'petalLength'} yDimension={'petalWidth'}/> -->
+
+<!-- <p><b>Commutes:</b> {$CommuteStore.length}</p> -->
 
 {$CommuteStore.length}
