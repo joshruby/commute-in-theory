afb17cefb3b9d3b7c0ececc1f19c7ed416378f0d
joshua-ruby <josh.r.ruby@gmail.com>
2021-12-25 20:28:29 -0800

    Rotate X axis tick labels

--- commute-in-theory-sveltekit/src/lib/components/ChartAxis.svelte
+++ commute-in-theory-sveltekit/src/lib/components/ChartAxis.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { select, selectAll, axisBottom, axisLeft } from 'd3'
+    import { select, selectAll, axisBottom, axisLeft, timeMinute, timeFormat} from 'd3'
 
     export let  innerHeight;
     export let  margin;
@@ -15,15 +15,37 @@
         let  axis;
         switch (position) {
             case "bottom":
-                axis = axisBottom(scale).tickSizeOuter(0);
+                axis = axisBottom(scale)
+                    .tickSizeOuter(0)
+                    .ticks(
+                        timeMinute.every(15),
+                        timeFormat("%H:%M")
+                    );
+                    
                 transform = `translate(0, ${innerHeight})`;
+
+                select(g).call(axis).selectAll("text")	
+                    .style("text-anchor", "end")
+                    .attr("dx", "-1em")
+                    .attr("dy", "0em")
+                    .attr("transform", "rotate(-65)");
+
                 break;
+
             case "left":
                 axis = axisLeft(scale).tickSizeOuter(0);
                 transform = `translate(${margin}, 0)`;
+
+                select(g).call(axis)
+
+                break;
         }
 
-        select(g).call(axis);
+        // select(g).call(axis).selectAll("text")	
+        //             .style("text-anchor", "end")
+        //             .attr("dx", "-.8em")
+        //             .attr("dy", ".15em")
+        //             .attr("transform", "rotate(-65)");;
     }
 </script>
 
