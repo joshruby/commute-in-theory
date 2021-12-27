0bcb3ae69fc0d9401cb5ab3de5f3fb143406e1ce
joshua-ruby <josh.r.ruby@gmail.com>
2021-12-26 19:04:23 -0800

    Reduce GET limit

--- commute-in-theory-sveltekit/src/routes/recorded-commutes/index.js
+++ commute-in-theory-sveltekit/src/routes/recorded-commutes/index.js
@@ -9,7 +9,7 @@ export async function get() {
         const collection = db.collection('commutes');
 
         // Retrieve db items and put into an array
-        const commutes = await collection.find().limit(20000).toArray();
+        const commutes = await collection.find().limit(10000).toArray();
 
         return {
             status: 200,
