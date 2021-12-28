1eab50f1126be64533188e0c63ec249d26ee8fa0
joshua-ruby <josh.r.ruby@gmail.com>
2021-12-28 11:27:58 -0800

    Modify tailwindcjs for v3.0, move layout to routes

---
+++ commute-in-theory-sveltekit/src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<div class="p-8 max-w-6xl">
+    <!-- The slot represents the components specific to each page -->
+    <slot></slot>
+</div>
+
+<style>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
