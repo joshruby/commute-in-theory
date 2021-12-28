4b4975a55c0692950f239997497e9b18ee365cd6
joshua-ruby <josh.r.ruby@gmail.com>
2021-12-28 08:53:46 -0800

    Working plot!

--- commute-in-theory-sveltekit/src/lib/components/ScatterChart.svelte
+++ commute-in-theory-sveltekit/src/lib/components/ScatterChart.svelte
@@ -1,42 +1,61 @@
 <script>
+    import { beforeUpdate, afterUpdate, onMount } from 'svelte';
     import { CommuteStore } from '$lib/stores/CommuteStore'
     import * as Plot from "@observablehq/plot";
 
     export let cityPair = 'CUP-SCZ';
 
+    // const data = [
+    //     {x: 1, y:100},
+    //     {x: 2, y:50},
+    //     {x: 3, y:200},
+    //     {x: 4, y:75},
+    //     {x: 5, y:100},
+    //     {x: 6, y:50},
+    //     {x: 7, y:200},
+    //     {x: 8, y:75},
+    // ];
+    
+    let data = [];
+    const max = 100;
+    for (let i = 0; i < 100; i++) {
+        data.push({
+            x: i,
+            y: Math.floor(Math.random() * (max + Math.pow(i, 2)))
+        })
+        
+    }
+
     const commutes = $CommuteStore[cityPair]
+    // console.log(commutes);
 
-    console.log(commutes)
-
-    let plot;
-
-    $: {
-        plot = Plot.plot({
+    let div;
+    
+    onMount(() => {
+        const svgPlot = Plot.plot({
             grid: true,
             marks: [
                 Plot.dot(
-                    commutes, 
-                    {x: "departureTime", y: "travelTimeInSeconds"}
+                    data, 
+                    {x: "x", y: "y"}
                 )
             ]
-        })
-    }
+        });
+
+        div.appendChild(svgPlot);
+    });
 </script>
 
-<svg>
-    <g bind:this={plot}></g>
-</svg>
-
-<!-- <svg height=600 width=600>
-  {plot}
-</svg> -->
-
-<!-- {plot} -->
+<div class="plot" bind:this={div} />
 
 <style>
-    svg {
+    div.plot {
         border: 1px solid red;
-        width: 900%;
-        height: 500px;
+        width: 800px;
+        height: 600px;
     }
+
+    /* svg {
+        border: 1px solid blue;
+    } */
 </style>
