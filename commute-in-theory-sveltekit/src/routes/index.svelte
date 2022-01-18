332bb06ce2e4a8c78870830e7066a79af0ad40e6
joshua-ruby <josh.r.ruby@gmail.com>
2022-01-17 22:37:47 -0800

    Update TODOs

--- commute-in-theory-sveltekit/src/routes/index.svelte
+++ commute-in-theory-sveltekit/src/routes/index.svelte
@@ -1,5 +1,6 @@
 <!-- TODO
-	- Store some data offline for use during developing. There's no sense in pulling the data each time when I'm not actively working on the GET processes
+	X Store some data offline for use during developing
+		- Import and parse the offline commutes
 
 	- Make a section for each city combination
 		- Only draw the plots when each section is opened 
