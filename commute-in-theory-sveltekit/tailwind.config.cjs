63b1560dc1b3ed040ac3e95e45c39390475829e1
joshua-ruby <jrruby@ucsd.edu>
2022-01-18 20:38:23 -0800

    Uninstall daisyui

--- commute-in-theory-sveltekit/tailwind.config.cjs
+++ commute-in-theory-sveltekit/tailwind.config.cjs
@@ -4,14 +4,5 @@ module.exports = {
   ],
   plugins: [
     require('@tailwindcss/typography'),
-    require('daisyui'),
   ],
-  daisyui: {
-    styled: true,
-    base: true,
-    utils: true,
-    // themes: true,
-    // logs: true,
-    // rtl: false,
-  },
 }
