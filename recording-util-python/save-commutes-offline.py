fbf90a4a6be543216eb224f068f1a9e2389f4c57
joshua-ruby <jrruby@ucsd.edu>
2022-01-19 20:51:52 -0800

    Localize offline commute timestamps to UTC

--- recording-util-python/save-commutes-offline.py
+++ recording-util-python/save-commutes-offline.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+import pytz
 from pymongo import MongoClient
 import config
 
@@ -7,16 +8,16 @@ client = MongoClient(config.MONGODB_URI)
 db = client.get_database()
 collection = db['commutes']
 
-docs = [doc for doc in collection.find({}).limit(35000)]
+docs = list(collection.find({}).limit(80000))
 for doc in docs:
     # Convert the id and departureTime to strings, so they can be serialized
     doc['_id'] = str(doc['_id'])
-    doc['departureTime'] = doc['departureTime'].isoformat()
+    doc['departureTime'] = pytz.utc.localize(doc['departureTime']).isoformat()
 
 commutes = {
     'saveTime': datetime.now().isoformat(),
     'commutes': docs
 }
 
-with open('commutes.json', 'w') as f:
+with open('commutes.json', 'w', encoding='utf-8') as f:
     json.dump(commutes, f)
