6d7b343dedaf2f6ef9e2d73829fa3770955662f3
joshua-ruby <jrruby@ucsd.edu>
2022-02-17 11:13:59 -0800

    Add title in-line overlapping with top border

--- commute-in-theory-sveltekit/src/lib/components/ChartWeekdaySelector.svelte
+++ commute-in-theory-sveltekit/src/lib/components/ChartWeekdaySelector.svelte
@@ -8,22 +8,29 @@
 </script>
 
 <!-- https://dev.to/thomasvanholder/click-label-to-choose-radio-button-tailwindcsss-peer-class-39nb -->
-<div class="grid grid-cols-3 divide-x w-80 py-3 border rounded-xl shadow-sm">
-    {#each weekdays as weekday}
-        <div class="text-center">
-            <input 
-                type="radio" 
-                bind:group={weekdaySelection} 
-                value={weekday} 
-                id={weekday + randStr}
-                class="peer sr-only"
-            >
-            <label 
-                for={weekday + randStr} 
-                class="peer-hover:text-baby-blue peer-checked:font-bold peer-checked:text-baby-blue ease-in-out duration-150"
-            >
-                {weekday}
-            </label>
+<div class="w-80 border rounded-xl shadow-sm">
+    <div class="grid grid-cols-1 place-items-center">
+        <div class="absolute top-1.5 px-3 bg-white font-bold">
+            Days
         </div>
-    {/each}
+    </div>
+    <div class="grid grid-cols-3 divide-x my-3">
+        {#each weekdays as weekday}
+            <div class="text-center">
+                <input
+                    type="radio"
+                    bind:group={weekdaySelection}
+                    value={weekday}
+                    id={weekday + randStr}
+                    class="peer sr-only"
+                >
+                <label
+                    for={weekday + randStr}
+                    class="peer-hover:text-baby-blue peer-checked:font-semibold peer-checked:text-baby-blue ease-in-out duration-150"
+                >
+                    {weekday}
+                </label>
+            </div>
+        {/each}
+    </div>
 </div>
