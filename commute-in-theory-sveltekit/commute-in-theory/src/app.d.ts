3adb2ca4cf3aa6fe1d50e616c07251523895fc4b
joshua-ruby <jrruby@ucsd.edu>
2023-08-06 12:48:16 -0700

    Add _mongoClientPromise to global typing

--- commute-in-theory-sveltekit/commute-in-theory/src/app.d.ts
+++ commute-in-theory-sveltekit/commute-in-theory/src/app.d.ts
@@ -1,3 +1,5 @@
+import type { MongoClient } from 'mongodb'
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
@@ -7,6 +9,9 @@ declare global {
 		// interface PageData {}
 		// interface Platform {}
 	}
+	namespace globalThis {
+		var _mongoClientPromise: Promise<MongoClient>
+	  }
 }
 
 export {};
