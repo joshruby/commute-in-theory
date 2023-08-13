a7769c9a61904b84ae433deee8a45cb88a4b532e
joshua-ruby <jrruby@ucsd.edu>
2023-08-13 16:24:51 -0700

    Improve WeekdaySelector screen size responsiveness

--- commute-in-theory-sveltekit/commute-in-theory/src/lib/Components/WeekdaySelector.svelte
+++ commute-in-theory-sveltekit/commute-in-theory/src/lib/Components/WeekdaySelector.svelte
@@ -8,13 +8,13 @@
 </script>
 
 <!-- https://dev.to/thomasvanholder/click-label-to-choose-radio-button-tailwindcsss-peer-class-39nb -->
-<div class="w-96 border rounded-xl shadow-sm">
+<div class="border rounded-xl shadow-sm">
     <div class="relative grid grid-cols-1 place-items-center">
         <div class="absolute -top-3 px-3 bg-white font-semibold text-slate-500">
             Days
         </div>
     </div>
-    <div class="grid grid-cols-3 divide-x my-3">
+    <div class="grid grid-cols-3 divide-x my-4">
         {#each weekdayGroups as weekday}
             <div class="text-center">
                 <input
@@ -26,14 +26,14 @@
                 >
                 <label
                     for={weekday + randStr}
-                    class="cursor-pointer text-slate-400 peer-hover:text-baby-blue peer-checked:font-bold peer-checked:text-baby-blue ease-in-out duration-150"
+                    class="mx-1 md:mx-4 cursor-pointer text-slate-400 peer-hover:text-baby-blue peer-checked:font-bold peer-checked:text-baby-blue ease-in-out duration-150"
                 >
                     {weekday}
                 </label>
             </div>
         {/each}
     </div>
-    <div class="grid grid-cols-7 divide-x my-3">
+    <div class="grid grid-cols-7 divide-x my-4">
         {#each weekdays as weekday}
             <div class="text-center">
                 <input
@@ -45,7 +45,7 @@
                 >
                 <label
                     for={weekday + randStr}
-                    class="cursor-pointer text-slate-400 peer-hover:text-baby-blue peer-checked:font-bold peer-checked:text-baby-blue ease-in-out duration-150"
+                    class="mx-1 md:mx-4 cursor-pointer text-slate-400 peer-hover:text-baby-blue peer-checked:font-bold peer-checked:text-baby-blue ease-in-out duration-150"
                 >
                     {weekday.slice(0,3)}
                 </label>
