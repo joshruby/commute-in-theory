7501e1d8674e683e6a1e9e9b6545a3c46874fcd9
joshua-ruby <jrruby@ucsd.edu>
2023-08-12 20:03:49 -0700

    Add individual weekday selection options

--- commute-in-theory-sveltekit/commute-in-theory/src/lib/Components/WeekdaySelector.svelte
+++ commute-in-theory-sveltekit/commute-in-theory/src/lib/Components/WeekdaySelector.svelte
@@ -1,20 +1,21 @@
 <script>
     export let weekdaySelection = 'All';
 
-    const weekdays = ['All', 'Business', 'Weekend']
+    const weekdayGroups = ['All', 'Business', 'Weekend']
+    const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     // Generate a random string for this component
     const randStr = Math.random().toString(36).replace(/[^a-z]+/g, '').substring(0, 5);
 </script>
 
 <!-- https://dev.to/thomasvanholder/click-label-to-choose-radio-button-tailwindcsss-peer-class-39nb -->
-<div class="w-80 border rounded-xl shadow-sm">
+<div class="w-96 border rounded-xl shadow-sm">
     <div class="relative grid grid-cols-1 place-items-center">
         <div class="absolute -top-3 px-3 bg-white font-semibold text-slate-500">
             Days
         </div>
     </div>
     <div class="grid grid-cols-3 divide-x my-3">
-        {#each weekdays as weekday}
+        {#each weekdayGroups as weekday}
             <div class="text-center">
                 <input
                     type="radio"
@@ -28,6 +29,25 @@
                     class="cursor-pointer text-slate-400 peer-hover:text-baby-blue peer-checked:font-bold peer-checked:text-baby-blue ease-in-out duration-150"
                 >
                     {weekday}
+                </label>
+            </div>
+        {/each}
+    </div>
+    <div class="grid grid-cols-7 divide-x my-3">
+        {#each weekdays as weekday}
+            <div class="text-center">
+                <input
+                    type="radio"
+                    bind:group={weekdaySelection}
+                    value={weekday}
+                    id={weekday + randStr}
+                    class="peer sr-only"
+                >
+                <label
+                    for={weekday + randStr}
+                    class="cursor-pointer text-slate-400 peer-hover:text-baby-blue peer-checked:font-bold peer-checked:text-baby-blue ease-in-out duration-150"
+                >
+                    {weekday.slice(0,3)}
                 </label>
             </div>
         {/each}
