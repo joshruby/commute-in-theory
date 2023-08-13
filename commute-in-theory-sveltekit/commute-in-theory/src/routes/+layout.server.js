dca9bffe929f96925435f7257b51b713370306b7
joshua-ruby <jrruby@ucsd.edu>
2023-08-12 18:37:31 -0700

    Simplify load, rm spinner (data loaded during SSR)

--- commute-in-theory-sveltekit/commute-in-theory/src/routes/+layout.server.js
+++ commute-in-theory-sveltekit/commute-in-theory/src/routes/+layout.server.js
@@ -1,15 +1,11 @@
 import * as db from '$lib/db';
-import { processCommuteStats } from '$lib/utils';
 
 export async function load() {
     const response = await db.getStats()
-    
+
     if (response.ok) {
-        const data = await response.json()
+        const stats = await response.json()
 
-        // Process the commutes
-        let processedCommuteStats = processCommuteStats(data.stats);
-
-        return { processedCommuteStats }
+        return { stats }
     }   
 }
