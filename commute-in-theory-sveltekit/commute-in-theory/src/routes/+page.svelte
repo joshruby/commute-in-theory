47129adecac4b8d7d461e3d411c210db4e3d97f0
joshua-ruby <jrruby@ucsd.edu>
2023-08-13 13:28:32 -0700

    Reinstate HomeCitySelector with multiple locations

--- commute-in-theory-sveltekit/commute-in-theory/src/routes/+page.svelte
+++ commute-in-theory-sveltekit/commute-in-theory/src/routes/+page.svelte
@@ -3,6 +3,7 @@
     import { processCommuteStats } from '$lib/utils';
     import { ProcessedCommuteStats } from '$lib/Stores/CommuteStore';
     import { CityPairs } from '$lib/Stores/LocationStore';
+    import HomeCitySelector from '$lib/Components/HomeCitySelector.svelte';
     import ChartCard from '$lib/Components/ChartCard.svelte';
     import CityPairComparisonChart from '$lib/Components/CityPairComparisonChart.svelte';
     import CityPairScatterChart from '$lib/Components/CityPairScatterChart.svelte';
@@ -15,17 +16,26 @@
     const processedCommuteStats = processCommuteStats(data.stats)
     ProcessedCommuteStats.set(processedCommuteStats);
 
+    let selectedCities = [];
+    $: selectedCityPairs = $CityPairs.filter(
+        cityPair => selectedCities.includes(cityPair.home.code)
+    );
+
     let containerWidth;
     $: chartWidth = containerWidth > 1024 ? 1024 : (containerWidth - 60)
 	let chartHeight = 400;
 </script>
 
 <div bind:clientWidth={containerWidth}>
-	<div class="grid grid-cols-1 place-items-center gap-4">	   
-        <ChartCard>
-            <CityPairComparisonChart cityPairs={$CityPairs} {chartWidth} {chartHeight} />
-        </ChartCard>
-        {#each $CityPairs as cityPair}
+	<div class="grid grid-cols-1 place-items-center gap-4">
+        <HomeCitySelector bind:selectedCities={selectedCities} />
+
+        {#if selectedCityPairs.length}
+            <ChartCard>
+                <CityPairComparisonChart cityPairs={selectedCityPairs} {chartWidth} {chartHeight} />
+            </ChartCard>
+        {/if}
+        {#each selectedCityPairs as cityPair}
             <ChartCard>
                 <CityPairScatterChart {cityPair} {chartWidth} {chartHeight} />
                 <div class="my-8 border-b w-3/4"></div>
